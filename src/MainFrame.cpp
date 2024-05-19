37cf9334f52bc48492b1df03c67274259d4a5428
MELANYGABRIELA.ALFARO <melanygabriela.alfaro@ucr.ac.cr>
2024-05-19 12:05:08 -0600

    dejando solo la ventanita

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -3,7 +3,4 @@
 
 MainFrame::MainFrame (const wxString&title):wxFrame(nullptr,wxID_ANY,title){
 
-    wxPanel* panel=new wxPanel(this);
-    wxButton* button=new wxButton(panel,wxID_ANY,"Buttom lol",wxPoint(150,50),wxSize(120,35));
-
 }
