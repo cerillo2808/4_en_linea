96dcdd99fec2d45d8f7abfd9b680f81e77fc183c
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-05-20 23:04:33 -0600

    da un mensaje cuando se presiona el boton

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -1,9 +1,19 @@
 #include <MainFrame.hh>
 #include <wx/wx.h>
 
+enum IDs {
+    BUTTON_ID = 2
+};
+
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+    EVT_BUTTON(BUTTON_ID, MainFrame::OnBottonClicked)
+wxEND_EVENT_TABLE()
+
 MainFrame::MainFrame (const wxString&title):wxFrame(nullptr,wxID_ANY,title){
 
     wxPanel* panel=new wxPanel(this);
-    wxButton* button=new wxButton(panel,wxID_ANY,"Buttom lol",wxPoint(150,50),wxSize(120,35));
+    wxButton* button=new wxButton(panel, BUTTON_ID,"Buttom lol",wxPoint(150,50),wxSize(120,35));
+
+    CreateStatusBar();
 
 }
