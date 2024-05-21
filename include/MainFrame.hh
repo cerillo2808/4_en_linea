d51145f6f58ad249c0ac180458cd7cc0ac800d6c
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-05-21 14:42:50 -0600

    copia manual del commit de Melany

--- include/MainFrame.hh
+++ include/MainFrame.hh
@@ -8,10 +8,6 @@ class MainFrame:public wxFrame
 public:
     //Constructor toma el nombre de la ventana como parametro
     MainFrame(const wxString& title);
-
-private:
-    void OnBottonClicked(wxCommandEvent & evt);
-    wxDECLARE_EVENT_TABLE();
 };
 
 #endif 
