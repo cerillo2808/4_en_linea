b2b647f1fb6b7c88a241ddea2acbb8aa40165783
LUIS EDUARDO VALVERDE SOZA <luis.valverdesoza@ucr.ac.cr>
2024-05-20 23:49:30 -0600

    Añadido de funciones basicos de tablero al header, e implemntación del constructor y retorno del tablero

--- include/Tablero.hh
+++ include/Tablero.hh
@@ -1,8 +1,24 @@
 #ifndef TABLERO_HH
 #define TABLERO_HH
 #include <iostream>
+# include <IJugador.hh>
 
 using namespace std;
 
+class Tablero {
+    public:
+    
+    //Constructor
+    Tablero (int filas, int columnas) {}
+    
+    Color ** getTablero () {}
 
+    bool analizarFichaJugadir (Color ficha) {}
+
+    void insertarFicha (Color Ficha) {} 
+
+    private: 
+
+    Color ** tablero;
+};
 #endif
--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -1,1 +1,16 @@
 #include <Tablero.hh>
+#include <IJugador.hh>
+
+Tablero :: Tablero (int filas, int columnas) {
+    tablero = new Color* [filas];
+
+    for (int i; i < filas; i++) {
+        tablero [i] = new Color [columnas];
+    } 
+}
+
+Color ** Tablero :: getTablero () {
+    return tablero;
+}
+
+// metodos de analizarFicha e insertarFicha están en proceso de desarrollo  
