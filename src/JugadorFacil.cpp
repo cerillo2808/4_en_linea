ceeae8902ce0bb33cb33d3451374b05d1b7f4b6c
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-05-21 14:24:03 -0600

    implementacion inicial del random

--- src/JugadorFacil.cpp
+++ src/JugadorFacil.cpp
@@ -1,6 +1,7 @@
 #include <JugadorFacil.hh>
 #include <IJugador.hh>
 #include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -10,5 +11,12 @@ JugadorFacil:: JugadorFacil (string nombre, Color ficha):
 
 
 void JugadorFacil:: jugar(){
+
+    std::random_device rd;
+    std::mt19937 engine(rd());
+    std::uniform_int_distribution<int> dist(1, 5);
+    //TODO: conectar la cantidad de columnas al random;
+    int numeroRandom = dist(engine);
+
     cout<<"IA fácil"<<endl;
 }
