7a8f280bacd0933effc7a87714deed5c46c336eb
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-05-21 17:32:49 -0600

    aplicar clang-format a JugadorFacil.cpp

--- src/JugadorFacil.cpp
+++ src/JugadorFacil.cpp
@@ -1,22 +1,19 @@
-#include <JugadorFacil.hh>
 #include <IJugador.hh>
+#include <JugadorFacil.hh>
 #include <iostream>
 #include <random>
 
 using namespace std;
 
-JugadorFacil:: JugadorFacil (string nombre, Color ficha):
-    IJugador (nombre,ficha)
-{}
+JugadorFacil::JugadorFacil(string nombre, Color ficha)
+    : IJugador(nombre, ficha) {}
 
+void JugadorFacil::jugar() {
+  std::random_device rd;
+  std::mt19937 engine(rd());
+  std::uniform_int_distribution<int> dist(1, 5);
+  // TODO: conectar la cantidad de columnas al random;
+  int numeroRandom = dist(engine);
 
-void JugadorFacil:: jugar  (){
-
-    std::random_device rd;
-    std::mt19937 engine(rd());
-    std::uniform_int_distribution<int> dist(1, 5);
-    //TODO: conectar la cantidad de columnas al random;
-    int numeroRandom = dist(engine);
-
-    cout<<"IA fácil"<<endl;
+  cout << "IA fácil" << endl;
 }
