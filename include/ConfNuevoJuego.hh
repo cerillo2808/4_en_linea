f98e6ecfda6e8b3ec7a6fd9bf6c57bb30f60f79c
MELANYGABRIELA.ALFARO <melanygabriela.alfaro@ucr.ac.cr>
2024-05-22 18:11:18 -0600

    menu para las configuraciones del juego

---
+++ include/ConfNuevoJuego.hh
@@ -0,0 +1,26 @@
+#ifndef CONFNUEVOJUEGO_HH
+#define CONFNUEVOJUEGO_HH
+
+#include <wx/wx.h>
+
+class ConfNuevoJuego: public wxFrame {
+    public:
+    //Constructor toma el nombre de la ventana como parametro
+    ConfNuevoJuego(const wxString& title);
+
+    private:
+    void jugadorUno(wxCommandEvent& event);
+    void jugadorDos(wxCommandEvent& event);
+    void filasTablero(wxCommandEvent& event);
+    void columnasTablero(wxCommandEvent& event);
+    void botonRegresar(wxCommandEvent& event);
+    void botonIniciar(wxCommandEvent& event);
+
+    int tipoJugadorUno;
+    int tipoJugadorDos;
+    int numFilasTablero;
+    int numColumnasTablero;
+};
+
+
+#endif
