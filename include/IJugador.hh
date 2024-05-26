fe715f393d74eef9c638c242feb1560b43628c3f
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-05-26 10:14:47 -0600

    implementacion de enum colores

--- include/IJugador.hh
+++ include/IJugador.hh
@@ -5,6 +5,12 @@
 using namespace std;
 
 enum Color{
+    azul = 1,
+    //azul será jugador 1
+    rojo = 2,
+    //rojo será jugador 2
+    non_color = 0
+    //el non_color es 0 porque es el default
 
 };
 
